1c4167602a4f381b88c0a0cd4b8ffe3090ffb5c3
Dscdago <daguito_saenz@hotmail.com>
2022-12-29 16:25:17 -0700

    fixed issue with models and migrated them

--- goodbank/admin.py
+++ goodbank/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, UserProfile, Accounts, CreditApp, CreditAccounts, Transactions
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(UserProfile)
+admin.site.register(Accounts)
+admin.site.register(CreditApp)
+admin.site.register(CreditAccounts)
+admin.site.register(Transactions)
--- goodbank/models.py
+++ goodbank/models.py
@@ -8,20 +8,20 @@ class User(AbstractUser):
 
 # User Profile. Extends User model
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="userProfile")
     userType = models.CharField(max_length=10)
     address = models.CharField(max_length = 800)
     phoneNumber = models.CharField(max_length=10)
     firstLogin = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"User Profile of {self.user}"
+        return f"User Profile of {self.user.username}"
 
 # Accounts Model
 class Accounts(models.Model):
     accountNumber = models.CharField(max_length=20)
-    balance = models.DecimalField(decimal_places=2)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="account")
+    balance = models.DecimalField(decimal_places=2, max_digits=10)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="userAccount")
 
     def __str__(self):
         return f"Account #{self.accountNumber} by {self.user.username} with ${self.balance}"
@@ -29,22 +29,22 @@ class Accounts(models.Model):
 # Credit Application Model
 class CreditApp(models.Model):
     status = models.CharField(max_length=6)
-    approvedLimit = models.DecimalField(decimal_places=2)
+    approvedLimit = models.DecimalField(decimal_places=2, max_digits=10)
     creditScore = models.IntegerField()
-    estimatedDebt = models.DecimalField(decimal_places=2)
-    yearlyIncome = models.DecimalField(decimal_places=2)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    estimatedDebt = models.DecimalField(decimal_places=2, max_digits=10)
+    yearlyIncome = models.DecimalField(decimal_places=2, max_digits=10)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="userCreditApp")
 
     def __str__(self):
         return f"Credit App for {self.user.username}. Status: {self.status}"
 
 # Credit Account Model
 class CreditAccounts(models.Model):
-    accountNumber = models.CharField(max_length=20)
-    balance = models.DecimalField(decimal_places=2)
-    creditLimit = models.DecimalField(decimal_places=2)
-    applicationID = models.ForeignKey(CreditApp, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    creditaccountNumber = models.CharField(max_length=20)
+    balance = models.DecimalField(decimal_places=2, max_digits=10)
+    creditLimit = models.DecimalField(decimal_places=2, max_digits=10)
+    applicationID = models.ForeignKey(CreditApp, on_delete=models.CASCADE, related_name="creditAccount")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="userCreditAcc")
     cardNumber = models.CharField(max_length=200)
 
     def __str__(self):
@@ -52,12 +52,12 @@ class CreditAccounts(models.Model):
 
 # Transactions Model
 class Transactions(models.Model):
-    amount = models.DecimalField(decimal_places=2)
+    amount = models.DecimalField(decimal_places=2, max_digits=10)
     date = models.DateTimeField(auto_now_add=True)
     details = models.CharField(max_length=800)
     transactionType = models.CharField(max_length=10)
-    accountNumber = models.ForeignKey(Accounts, on_delete=models.CASCADE)
-    cardNumber = models.ForeignKey(CreditAccounts, on_delete=models.CASCADE)
+    accountNumber = models.ForeignKey(Accounts, on_delete=models.CASCADE, related_name="accountTransaction")
+    cardNumber = models.ForeignKey(CreditAccounts, on_delete=models.CASCADE, related_name="cardTransaction")
 
     def __str__(self):
         return f"Transaction #{self.id}. Type: {self.transactionType}"
